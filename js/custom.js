9afc70d650c9086b2ee2412de7764170fde4a3c8
hazelnut-infinity <apondihazel@gmail.com>
2017-05-31 03:10:24 +0000

    My Personal Profile,Updates and Changes

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,3 @@
-/* ========================================================================= */
-/*	Preloader
-/* ========================================================================= */
-
-jQuery(window).load(function(){
-
-	$("#preloader").fadeOut("slow");
-
-});
-
-
 $(document).ready(function(){
 
 	/* ========================================================================= */
@@ -32,7 +21,7 @@ $(document).ready(function(){
 	
     $(window).scroll(function () {
         if ($(window).scrollTop() > 400) {
-            $("#navigation").css("background-color","#0EB493");
+            $("#navigation").css("background-color","#9E9E9E");
         } else {
             $("#navigation").css("background-color","rgba(16, 22, 54, 0.2)");
         }
@@ -50,51 +39,6 @@ $(document).ready(function(){
 		$('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height',slideHeight);
 	});
 	
-	
-	/* ========================================================================= */
-	/*	Portfolio Filtering
-	/* ========================================================================= */	
-	
-	
-    // portfolio filtering
-
-    $(".project-wrapper").mixItUp();
-	
-	
-	$(".fancybox").fancybox({
-		padding: 0,
-
-		openEffect : 'elastic',
-		openSpeed  : 650,
-
-		closeEffect : 'elastic',
-		closeSpeed  : 550,
-
-		closeClick : true,
-	});
-	
-	/* ========================================================================= */
-	/*	Parallax
-	/* ========================================================================= */	
-	
-	$('#facts').parallax("50%", 0.3);
-	
-	/* ========================================================================= */
-	/*	Timer count
-	/* ========================================================================= */
-
-	"use strict";
-    $(".number-counters").appear(function () {
-        $(".number-counters [data-to]").each(function () {
-            var e = $(this).attr("data-to");
-            $(this).delay(6e3).countTo({
-                from: 50,
-                to: e,
-                speed: 3e3,
-                refreshInterval: 50
-            })
-        })
-    });
 	
 	/* ========================================================================= */
 	/*	Back to Top
@@ -117,35 +61,3 @@ $(document).ready(function(){
 });
 
 
-// ==========  START GOOGLE MAP ========== //
-function initialize() {
-    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
-
-    var mapOptions = {
-        zoom: 14,
-        center: myLatLng,
-        disableDefaultUI: true,
-        scrollwheel: false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        draggable: false,
-        mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
-        }
-    };
-
-    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
-
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: 'img/location-icon.png',
-        title: '',
-    });
-
-}
-
-google.maps.event.addDomListener(window, "load", initialize);
-// ========== END GOOGLE MAP ========== //
